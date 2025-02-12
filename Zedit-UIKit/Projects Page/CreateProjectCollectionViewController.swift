9e0552377a5266b3f41b0edabdeb991e37376c19
Avinash96-gthb <avinash.a2k4@gmail.com>
2025-02-12 19:23:15 +0530

    FEAT: fixed keyboard pop up

--- Zedit-UIKit/Projects Page/CreateProjectCollectionViewController.swift
+++ Zedit-UIKit/Projects Page/CreateProjectCollectionViewController.swift
@@ -293,16 +293,16 @@ extension CreateProjectCollectionViewController {
 // MARK: - TextField and Keyboard Handling
 extension CreateProjectCollectionViewController {
     @objc func keyboard(notification: Notification) {
-        guard let keyboardRect = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue else {
-            return
-        }
-        
-        if notification.name == UIResponder.keyboardWillShowNotification ||
-            notification.name == UIResponder.keyboardWillChangeFrameNotification {
-            view.frame.origin.y = -keyboardRect.height
-        } else {
-            view.frame.origin.y = 0
-        }
+//        guard let keyboardRect = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue else {
+//            return
+//        }
+//        
+//        if notification.name == UIResponder.keyboardWillShowNotification ||
+//            notification.name == UIResponder.keyboardWillChangeFrameNotification {
+//            view.frame.origin.y = -keyboardRect.height
+//        } else {
+//            view.frame.origin.y = 0
+//        }
     }
     
     @objc func textFieldDidChange(_ textField: UITextField) {
